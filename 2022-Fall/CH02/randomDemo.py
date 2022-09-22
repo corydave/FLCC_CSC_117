574c11e70f4524a7064fe02bf77062cfb77201b4
corydave <corydave@users.noreply.github.com>
2022-09-22 13:32:14 -0400

    Create randomDemo.py

---
+++ 2022-Fall/CH02/randomDemo.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+# [0, 1)
+
+# print(int(random.random() * 10))
+# print(int(random.random() * 10))
+# print(int(random.random() * 10))
+
+# print(random.randrange(1, 7))
+# print(random.randrange(1, 6))
+
+# print(random.choice('abc'))
+
+# print(random.choice(string.ascii_letters))
+
+# specialCharacters = '!@*-_[]'
+# print(random.choice(specialCharacters))
+
+# random.seed(32)
+
+print(random.random())
+print(random.random())
+print(random.random())
+
+
+
+
+
+
+
