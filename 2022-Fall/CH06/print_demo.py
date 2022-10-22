14030d3c3b65f4dc366b3b9f276362b14aab8fb1
corydave <corydave@users.noreply.github.com>
2022-10-22 12:08:04 -0400

    Create print_demo.py

---
+++ 2022-Fall/CH06/print_demo.py
@@ -0,0 +1,13 @@
+def main():
+    print_demo()
+
+def print_demo():
+    print('a', 'b', 'c')
+    print('a' + 'b' + 'c')
+
+    print('a' + '\n' + 'b')
+
+    print('dave ', end='')
+    print('ghidiu')
+
+main()
